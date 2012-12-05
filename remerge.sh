bf15717c54f16ef22cc8e7bb61fe39aa18367fff
Sergey Lapin <slapin@ossfans.org>
2012-12-05 19:21:32 +0400

    Added q2tools

--- remerge.sh
+++ remerge.sh
@@ -5,6 +5,7 @@ git remote add q2game git@github.com:slapin/quake2-game-so.git
 git remote add netradiant git://git.icculus.org/divverent/netradiant.git
 git remote add netradiant2 git://github.com/clbr/netradiant.git
 git remote add netradiant3 git://github.com/divVerent/NetRadiant.git
+git remote add q2tools git://github.com/id-Software/Quake-2-Tools
 git remote update
 
 git merge -s subtree q2/master
@@ -13,4 +14,5 @@ git merge -s subtree gtkradiant/master
 git merge -s subtree netradiant/master
 git merge -s subtree netradiant2/master
 git merge -s subtree netradiant3/master
+git merge -s subtree q2tools/master
 
