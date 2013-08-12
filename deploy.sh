6accb0be1cd0b2ad1822f37f5e198dbe62aa4628
Sergey Lapin <slapin@ossfans.org>
2013-08-12 06:55:53 +0400

    rename of run.sh to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,11 @@ install -c -m 644 ./testmaps/default.cfg ./deploy/testgame
 install -m 755 -d ./deploy/testgame/maps
 install -m 755 -d ./deploy/testgame/textures
 install -m 755 -d ./deploy/testgame/pics
+install -m 755 -d ./deploy/testgame/env
 cp -a ./testmaps/maps/*.bsp ./deploy/testgame/maps/
 cp -a ./testmaps/textures/* ./deploy/testgame/textures/
 cp -a ./testmaps/pics/* ./deploy/testgame/pics/
+cp -a ./testmaps/env/* ./deploy/testgame/env/
 rm -Rf ./deploy/radiant
 cp -a ./netradiant3/install ./deploy/radiant
 install -d -m 755 ./deploy/radiant/games
